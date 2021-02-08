bfddfa5719d489e32773cf0ca0126b7127d1f6c3
Vladimir Kraus <vladimir-kraus@users.noreply.github.com>
2021-02-08 20:42:05 +0100

    Create leakcheck.cpp

---
+++ cpputils/leakcheck.cpp
@@ -0,0 +1,89 @@
+#include "leakcheck.h"
+
+#if defined(LEAKCHECK_ENABLED)
+
+#include <cassert>
+#include <iostream>
+
+std::map<std::string, LeakChecker::Entry> LeakChecker::s_stats;
+
+std::mutex LeakChecker::s_mutex;
+
+void LeakChecker::update(const char *typeName, int increment)
+{
+    assert(increment == 1 || increment == -1);
+
+    const std::lock_guard<std::mutex> lock(s_mutex);
+
+    Entry &e = s_stats[std::string(typeName)];
+
+    e.count += increment;
+    assert(e.count >= 0);
+
+    if (increment > 0)
+    {
+        e.total += increment;
+    }
+
+    if (e.count > e.max)
+    {
+        e.max = e.count;
+    }
+}
+
+void LeakChecker::print_leaks()
+{
+    const std::lock_guard<std::mutex> lock(s_mutex);
+
+    std::cout << "Leak-checker leaks (type, count):" << std::endl;
+    int leaks = 0;
+
+    for (const auto & kv : s_stats)
+    {
+        if (kv.second.count > 0)
+        {
+            std::cout << kv.first << ", " << kv.second.count << std::endl;
+            leaks += kv.second.count;
+        }
+    }
+
+    if (leaks == 0)
+    {
+        std::cout << "no leaks" << std::endl;
+    }
+    else
+    {
+        std::cout << leaks << " leaks found" << std::endl;
+    }
+
+    std::cout << std::endl;
+}
+
+void LeakChecker::print_stats()
+{
+    const std::lock_guard<std::mutex> lock(s_mutex);
+
+    std::cout << "Leak-checker stats (type, count, total, max):" << std::endl;
+
+    for (const auto & kv : s_stats)
+    {
+        std::cout << kv.first << ", " << kv.second.count << ", " << kv.second.total << ", " << kv.second.max << std::endl;
+    }
+
+    std::cout << std::endl;
+}
+
+bool LeakChecker::has_leaks()
+{
+    for (const auto & kv : s_stats)
+    {
+        if (kv.second.count > 0)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+#endif
